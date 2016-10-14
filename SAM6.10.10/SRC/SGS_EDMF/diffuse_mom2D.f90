ed6115625a42e75ae847a00fcea0111c16250123
Wolfgang Langhans <wlanghans@lbl.gov>
2016-10-13 17:19:49 -0700

    fixed sgs fluxes in stats giagnostic output

--- SAM6.10.10/SRC/SGS_EDMF/diffuse_mom2D.f90
+++ SAM6.10.10/SRC/SGS_EDMF/diffuse_mom2D.f90
@@ -58,14 +58,14 @@ do i=1,nx
     tk1D = 0.5*(tk(i,j,1:nzm)+tk(ib,j,1:nzm))
     call get_abcd(i,j,betap,betam,var,sumMs1D,tk1D,a,b,c,d, massflux, fluxbu(i,j))
     call tridiag(a,b,c,d)
-    uwsb(1) = uwsb(1) + fluxbu(i,j)
+    uwsb(1) = uwsb(1) + fluxbu(i,j) * rhow(1) / dz
     uwsb3(i,j,1) = fluxbu(i,j)
     uwsb3(i,j,nz) = 0.0
     uwsb(nz)= 0.
     uwsb3(i,j,2:nzm) =  (-1.) /adzw(2:nzm)/dz *         0.25*(tk(i,j,1:nzm-1) + tk(i,j,2:nzm)+tk(ib,j,1:nzm-1) + tk(ib,j,2:nzm)) *                  &
                            (betap* (d(2:nzm)- d(1:nzm-1))+betam*(u(i,j,2:nzm)-u(i,j,1:nzm-1)) ) &
                            +(0.5*(sgs_field_sumM(i,j,2:nzm,2)+sgs_field_sumM(ib,j,2:nzm,2)) - (betap*d(2:nzm) + betam*u(i,j,2:nzm)) * sgs_field_sumM(i,j,2:nzm,1) )
-    uwsb(2:nzm) = uwsb(2:nzm) + rhow(2:nzm) * uwsb3(i,j,2:nzm)
+    uwsb(2:nzm) = uwsb(2:nzm) + rhow(2:nzm) * uwsb3(i,j,2:nzm) / dz
     dudtdiff(i,j,:,na) = (d-u(i,j,:))/dtn
     dudt(i,j,:,na) = dudt(i,j,:,na) + dudtdiff(i,j,:,na)
 end do
@@ -79,14 +79,14 @@ do i=1,nx
     tk1D = tk(i,j,1:nzm)
     call get_abcd(i,j,betap,betam,var,sumMs1D,tk1D,a,b,c,d, massflux, fluxbv(i,j))
     call tridiag(a,b,c,d)
-    vwsb(1) = vwsb(1) + fluxbv(i,j)
+    vwsb(1) = vwsb(1) + fluxbv(i,j) * rhow(1) / dz
     vwsb3(i,j,1) = fluxbv(i,j)
     vwsb3(i,j,nz) = 0.0
     vwsb(nz)= 0.
     vwsb3(i,j,2:nzm) =  (-1.) /adzw(2:nzm)/dz *         0.5*(tk(i,j,1:nzm-1) + tk(i,j,2:nzm)) *                  &
                            (betap* (d(2:nzm)- d(1:nzm-1))+betam*(v(i,j,2:nzm)-v(i,j,1:nzm-1)) ) &
                            +(sgs_field_sumM(i,j,2:nzm,3) - (betap*d(2:nzm) + betam*v(i,j,2:nzm)) * sgs_field_sumM(i,j,2:nzm,1) )
-    vwsb(2:nzm) = vwsb(2:nzm) + rhow(2:nzm) * vwsb3(i,j,2:nzm)
+    vwsb(2:nzm) = vwsb(2:nzm) + rhow(2:nzm) * vwsb3(i,j,2:nzm) / dz
     dvdtdiff(i,j,:,na) = (d-v(i,j,:))/dtn
     dvdt(i,j,:,na) = dvdt(i,j,:,na) + dvdtdiff(i,j,:,na)
 end do
